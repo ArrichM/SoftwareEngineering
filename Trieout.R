f673116613474753077da6c04f4ab372c11625e3
Max Arrich <max_x@live.at>
2018-11-20 20:19:45 +0100

    played arounf with variables

--- Trieout.R
+++ Trieout.R
@@ -8,11 +8,13 @@ library(magrittr)
 library(xts)
 library(stats)
 library(tseries)
+
 stdz <- function(x) (x-mean(x,na.rm=T))/sd(x,na.rm=T)
+
 get.eurounemp <- function(){
-  search_eurostat("Arbeitslosengeld", fixed = F)$code #namq_10_gdp
+  search_eurostat("unemployment", fixed = F)$code #namq_10_gdp
   unemp <- get_eurostat("une_rt_m", select_time = "M")
-  unemp <- subset(unemp, geo == "AT" & sex == "T" & unit == "PC_ACT" & age == "TOTAL" & s_adj == "NSA")
+  unemp <- subset(unemp, geo == "UK" & sex == "T" & unit == "PC_ACT" & age == "TOTAL" & s_adj == "NSA")
   unemp <- label_eurostat(unemp, lang = "de")
   unemp$time <- as.POSIXct.Date(unemp$time)
   unemp.ts <- ts(rev(unemp$values), start = c(as.numeric(tail(substr(unemp$time,1,4),1)),
@@ -20,6 +22,7 @@ get.eurounemp <- function(){
   unemp.ts <- unemp.ts/100
   return(unemp.ts)
 }
+
 get.eurocons <- function(){
   search_eurostat("consumption", fixed = F)$code #namq_10_fcs
   privcons <- get_eurostat("namq_10_fcs", select_time = "Q")
@@ -30,19 +33,21 @@ get.eurocons <- function(){
   privcons$time <- as.POSIXct.Date(privcons$time)
   privcons.ts <- ts(rev(privcons$values), start = c(as.numeric(tail(substr(privcons$time,1,4),1)),
                                                     as.numeric(tail(substr(privcons$time,6,7),1))), freq = 4)
-  privcons.ts <- diff(privcons.ts, lag = 4)/privcons.ts
+  privcons.ts <- diff(privcons.ts, lag = 1)/privcons.ts
   return(privcons.ts)
 }
-freq_use <- 12L
+
+freq_use <- 12
 #query the google api for keywords
-query <- gtrends(c("Arbeitslosengeld"),geo = "AT", gprop = "web", time = "all")
+query <- gtrends(c("job"),geo = "GB", gprop = "web", time = "all")
 #query2 <-  gtrends(c("Zalando"),geo = "AT", gprop = "web", time = "all")
 tophits <- query$related_queries$value[1:5]
+tophits
 #tophits2 <- query2$related_queries$value[1:5]
 
 #get series for keywords separately to get full granity of each series
 google_multiple <- function(tophits){
-  query_multiple <- lapply(tophits, function(x) gtrends(x,geo = "AT", gprop = "web", time = "all")[[1]] %>%  
+  query_multiple <- lapply(tophits, function(x) gtrends(x,geo = "GB", gprop = "web", time = "all")[[1]] %>%  
                              dcast(date ~ keyword + geo, value.var = "hits"))
   #transform output to ts
   query_multiple_ts <- lapply(query_multiple, function(x) ts(x[,2],start =c(year(x$date[1]),month(x$date[1])), frequency = 12))
@@ -58,6 +63,8 @@ query_multiple_ts_bound <- google_multiple(tophits)
 
 query_multiple_ts_bound <- ts(apply(query_multiple_ts_bound,2,function(x) as.numeric(gsub("<","",as.character(x)))),start=start(query_multiple_ts_bound))
 
+plot(query_multiple_ts_bound)
+
 #do PCA of the time series and get predicted PCs
 PCA_query <- prcomp((query_multiple_ts_bound)) #insert diff here if differentiation is desired
 comps_ts <- ts(predict(PCA_query), start = start(query_multiple_ts_bound), frequency = 12)
@@ -71,7 +78,7 @@ unemp = get.eurounemp()
 #unemp = get.eurocons()
 
 #create lagged set and set colnames
-nlags <- 1:1
+nlags <- 1:9
 
 comp_lags <- do.call(cbind,lapply(nlags, function(x) lag(comps_ts,-x)))
 colnames(comp_lags) <- paste0(rep(colnames(comps_ts),length(nlags)),"-L",rep(nlags,each = 5))
@@ -86,15 +93,13 @@ full.pca <- lm((unemp_window)~.,data=comp_lags_window)
 empty.pca <- lm((unemp_window)~1,data=comp_lags_window)
 
 #run stepwise selection on the lagged PCs
-pca.model <- step(empty.pca, scope=list(lower=empty.pca, upper=full.pca) , direction = "both")
+pca.model <- step(empty.pca, scope=list(lower=empty.pca, upper=full.pca) , direction = "both", k = log(ncol(comp_lags_window)))
 summary(pca.model)
 
 #produce fitted values
 point_estimate <- na.remove(ts(predict(pca.model,newdata = comp_lags), start = start(comp_lags),freq = freq_use))
-plot(point_estimate)
-lines(unemp, col = "red")
-
-plot((pexp(rexp(1000,1))))
+plot(unemp)
+lines(point_estimate, col = "red")
 
 
 
