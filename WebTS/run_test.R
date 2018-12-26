896945c3817f25976dee6f47fe46bc6fc88d33c4
Max <Max@Umpas-MacBook-Pro.local>
2018-12-26 14:36:52 +0100

    Added secon more complex test for recursive multiplication

--- WebTS/run_test.R
+++ WebTS/run_test.R
@@ -1,12 +1,18 @@
-toload <- c("magrittr","reshape2","lubridate","zoo","tseries","dyn","testthat")
+toload <- c("magrittr","reshape2","lubridate","zoo","tseries","dyn","testthat","rstudioapi")
 toinstall <- toload[which(toload %in% installed.packages()[,1] == F)] #which packages are not already installed?
 lapply(toinstall, install.packages, character.only = TRUE) #install missing packages
 lapply(toload, require, character.only = TRUE) #load packages
 
-source("resources.R")
-source("test_resources.R")
+# set correct wd
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
 
+# source script containing functions to test
+source("resources.R")
+
+# run tests
 test_results <- test_dir(getwd(), reporter="summary")
 
+# test results
 print(test_results)
 
--- WebTS/test_resources.R
+++ WebTS/test_resources.R
@@ -8,3 +8,11 @@ test_that("Test multiply_recursive() Constant Growth",{
   expect_equal(as.numeric(tail(multiply_recursive(test_level_ts,test_growth_ts),1)),constant_11_periods)
   
 })
+
+test_that("Test multiply_recursive()  Varying Growth",{
+  test_level_ts <- ts(c(10,10*1.08,10*1.08*1.03)) #empiric levels in first 3 periods
+  test_growth_ts <- ts(c(NA,0.08,0.03,0.07,0.02,0.04))  #empiric growth in first 3 and then 2 and 4 percent
+  expected <- c(10.8,11.124,11.90268,12.1407336,12.62636294) #manually calculated values. We lose first observation due to differentiation
+  expect_equal(as.numeric(multiply_recursive(test_level_ts,test_growth_ts)),expected)
+  
+})
